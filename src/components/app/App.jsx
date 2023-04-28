633f9579b1ea0d10b5194c973d7893678a29fc38
Gesiel Gimenez <gesielgimenez@gmail.com>
2023-04-28 15:42:11 -0500

    inicio

---
+++ src/components/app/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "../header/Header";
+import Main from "../main/Main";
+import GlobalStyles from "../StyleApp";
+
+const App = () => {
+  return (
+    <div>
+       <Header/>
+       <Main/>
+       <GlobalStyles/>
+    </div>
+  );
+};
+
+export default App;
